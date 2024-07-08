7c57dfeee4731d057fb254d6c40e445d114c15fd
benfic4rthur <arthur_benfica@hotmail.com>
2024-07-08 14:31:43 -0300

    aprendendo ngStyle.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,39 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'projeto-ngstyle';
+  fontColor: 'black' | 'red' | 'green' | 'blue' | 'orange' | 'purple' = 'black';
+  buttonText: 'Preto' | 'Vermelho' | 'Verde' | 'Azul' | 'Laranja' | 'Roxo' =
+    'Vermelho';
+  fontSize: number = 20;
+
+  mudaTamanho() {
+    this.fontSize += 1;
+    if (this.fontSize >= 40) {
+      this.fontSize = 20;
+    }
+  }
+  mudaCor() {
+    if (this.fontColor === 'black') {
+      this.fontColor = 'red';
+      this.buttonText = 'Verde';
+    } else if (this.fontColor === 'red') {
+      this.fontColor = 'green';
+      this.buttonText = 'Azul';
+    } else if (this.fontColor === 'green') {
+      this.fontColor = 'blue';
+      this.buttonText = 'Laranja';
+    } else if (this.fontColor === 'blue') {
+      this.fontColor = 'orange';
+      this.buttonText = 'Roxo';
+    } else if (this.fontColor === 'orange') {
+      this.fontColor = 'purple';
+      this.buttonText = 'Preto';
+    } else if (this.fontColor === 'purple') {
+      this.fontColor = 'black';
+      this.buttonText = 'Vermelho';
+    }
+  }
 }
